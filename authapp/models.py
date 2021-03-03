48e19c7c94d1b8f2cb6ab67f2221d509f0039404
nat0t <nataniel1987@gmail.com>
2021-03-03 21:45:51 +0300

    Created application 'authapp'. Added model 'ShopUser'.

---
+++ authapp/models.py
@@ -0,0 +1,5 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+class ShopUser(AbstractUser):
+    avatar = models.ImageField(upload_to='users_avatars', blank=True)
