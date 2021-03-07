cd3ee09cc19c71dd2de2c588fda39732034139c3
nat0t <nataniel1987@gmail.com>
2021-03-07 15:23:10 +0300

    Added template for profile.

--- authapp/views.py
+++ authapp/views.py
@@ -33,3 +33,6 @@ def register(request):
 def logout(request):
     auth.logout(request)
     return HttpResponseRedirect(reverse('index'))
+
+def profile(request):
+    return render(request, 'authapp/profile.html')
