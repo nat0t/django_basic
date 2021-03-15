7bada3d2998b27be4d7f68e5f011f36906035512
nat0t <nataniel1987@gmail.com>
2021-03-15 19:23:46 +0300

    Added ProductAdmin for Django.

--- mainapp/admin.py
+++ mainapp/admin.py
@@ -4,4 +4,12 @@ from .models import ProductsCategory, Product
 
 # Register your models here.
 admin.site.register(ProductsCategory)
-admin.site.register(Product)
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('name', 'price', 'quantity', 'category')
+    fields = ('name', 'image', 'description', 'short_description',
+              ('price','quantity'), 'category')
+    readonly_fields = ('short_description',)
+    ordering = ('name', 'price')
+    search_fields = ('name',)
