b86c31138847cdfbc9e788d8fa42d25280c7858c
nat0t <nataniel1987@gmail.com>
2021-03-15 04:49:53 +0300

    Created controller for create users.

---
+++ adminapp/forms.py
@@ -0,0 +1,17 @@
+from django import forms
+
+from authapp.forms import UserRegisterForm
+from authapp.models import ShopUser as User
+
+class UserAdminRegistrationForm(UserRegisterForm):
+    avatar = forms.ImageField(widget=forms.FileInput(), required=False)
+
+    class Meta:
+        model = User
+        fields = (
+        'username', 'email', 'avatar', 'first_name', 'last_name', 'password1',
+        'password2')
+
+    def __init__(self, *args, **kwargs):
+        super(UserAdminRegistrationForm, self).__init__(*args, **kwargs)
+        self.fields['avatar'].widget.attrs['class'] = 'custom-file-input'
