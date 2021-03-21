21e9fc2a9e0a79a2ea6ec60537a0f3b4109d5e16
nat0t <nataniel1987@gmail.com>
2021-03-21 09:52:07 +0300

    Fixed topics in adminapp. Added decorators to adminapp/views.py.

--- adminapp/views.py
+++ adminapp/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, HttpResponseRedirect
 from django.urls import reverse, reverse_lazy
 from django.contrib.auth.decorators import user_passes_test
+from django.utils.decorators import method_decorator
 from django.views.generic.list import ListView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 
@@ -11,12 +12,21 @@ from mainapp.models import Product
 
 @user_passes_test(lambda u: u.is_superuser, login_url='/')
 def index(request):
-    return render(request, 'adminapp/index.html')
+    context = {'title': 'GeekShop - Админка'}
+    return render(request, 'adminapp/index.html', context)
 
 # READ
 class UserListView(ListView):
     model = User
     template_name = 'adminapp/admin-users-read.html'
+    def get_context_data(self, **kwargs):
+        context = super(UserListView, self).get_context_data(**kwargs)
+        context['title'] = 'GeekShop - Пользователи'
+        return context
+
+    @method_decorator(user_passes_test(lambda u: u.is_superuser, login_url='/'))
+    def dispatch(self, request, *args, **kwargs):
+        return super(UserListView, self).dispatch(request, *args, **kwargs)
 
 # CREATE
 class UserCreateView(CreateView):
@@ -24,6 +34,14 @@ class UserCreateView(CreateView):
     template_name = 'adminapp/admin-users-create.html'
     form_class = UserAdminRegistrationForm
     success_url = reverse_lazy('admin-staff:admin_users')
+    def get_context_data(self, **kwargs):
+        context = super(UserCreateView, self).get_context_data(**kwargs)
+        context['title'] = 'GeekShop - Создание пользователя'
+        return context
+
+    @method_decorator(user_passes_test(lambda u: u.is_superuser, login_url='/'))
+    def dispatch(self, request, *args, **kwargs):
+        return super(UserCreateView, self).dispatch(request, *args, **kwargs)
 
 # UPDATE
 class UserUpdateView(UpdateView):
@@ -31,6 +49,15 @@ class UserUpdateView(UpdateView):
     template_name = 'adminapp/admin-users-update-delete.html'
     form_class = UserAdminProfileForm
     success_url = reverse_lazy('admin_staff:admin_users')
+
+    def get_context_data(self, **kwargs):
+        context = super(UserUpdateView, self).get_context_data(**kwargs)
+        context['title'] = 'GeekShop - Редактирование пользователя'
+        return context
+
+    @method_decorator(user_passes_test(lambda u: u.is_superuser, login_url='/'))
+    def dispatch(self, request, *args, **kwargs):
+        return super(UserUpdateView, self).dispatch(request, *args, **kwargs)
 
 # DELETE
 class UserDeleteView(DeleteView):
