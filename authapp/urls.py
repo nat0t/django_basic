c705ecd62b69d0100f73b950d7d550e5eb77f358
nat0t <nataniel1987@gmail.com>
2021-03-21 10:47:24 +0300

    Added CBV to authapp.

--- authapp/urls.py
+++ authapp/urls.py
@@ -1,12 +1,12 @@
 from django.urls import path
 
-from authapp.views import login, register, logout, profile
+from authapp.views import Login, Register, logout, profile
 
 app_name = 'authapp'
 
 urlpatterns = [
-    path('login/', login, name='login'),
-    path('register/', register, name='register'),
+    path('login/', Login.as_view(), name='login'),
+    path('register/', Register.as_view(), name='register'),
     path('logout/', logout, name='logout'),
     path('profile/', profile, name='profile'),
 ]
